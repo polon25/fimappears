6d0216334ee82283172c8bfc34eb631c89c1d53a
polon25 <jacek001@wp.pl>
2023-06-20 23:46:50 +0200

    Addedd example data

---
+++ R/fim.main.chars.R
@@ -0,0 +1,11 @@
+#' Main characters of MLP:FiM TV Series
+#'
+#' Data contains names of six main characters of MLP:FiM TV Series
+#'
+#' @docType data
+#' @usage data (fim.main.chars)
+#' @format vector
+#' @keywords datasets mlp fim
+#' @examples
+#' data(fim.main.chars)
+#' fim.chars.appears.df <- char.appears(fim.main.chars,get.webpage())
